81c53224200f2d68577d250b7fca0f6e634a02f1
Sahus Nulu <53015701+Insainian@users.noreply.github.com>
2023-11-08 20:28:40 -0800

    Create sample data.

--- Phase 2/SQLite.py
+++ Phase 2/SQLite.py
@@ -189,15 +189,307 @@ def dropTables(_conn):
     print(separator)
 
 
+# -------- INSERT STATEMENTS ----------
+def insertCourseData(_conn):
+    print(separator)
+    print("Insert Course Data")
+
+    try:
+        # Insert data into Course
+        sql = """INSERT INTO Course (courseID, teacherID, name, semester, section)
+              VALUES (1, 1, 'CSE 111', 'Spring', '4D')"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Course (courseID, teacherID, name, semester, section)
+              VALUES (2, 2, 'CSE 150', 'Spring', '3D')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Course (courseID, teacherID, name, semester, section)
+              VALUES (3, 3, 'CSE 175', 'Fall', '2D')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Course (courseID, teacherID, name, semester, section)
+              VALUES (4, 4, 'CSE 185', 'Fall', '46')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Course (courseID, teacherID, name, semester, section)
+              VALUES (5, 5, 'CSE 108', 'Spring', '4D')"""
+        _conn.execute(sql)
+
+        print("Success")
+    except Error as err:
+        print("Error: ", err)
+
+    print(separator)
+
+def insertTeacherData(_conn):
+    print(separator)
+    print("Insert Teacher Data")
+
+    try:
+        # Insert data into Teacher
+        sql = """INSERT INTO Teacher (teacherID, name, rating, email, office, title)
+               VALUES (1, 'Derek Sollberger', 5, 'dsollberger@example.com', 'cob 105', 'Professor')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Teacher (teacherID, name, rating, email, office, title)
+               VALUES (2, 'Florin Rusu', 5, 'frusu@exampl.com', 'cob 1', 'Professor')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Teacher (teacherID, name, rating, email, office, title)
+               VALUES (3, 'Angelo Kyrilov', 5, 'akyrilov@example.com', 'tesla', 'Professor')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Teacher (teacherID, name, rating, email, office, title)
+               VALUES (4, 'Sungjin Im', 5, 'sim@example.com', 'NaN', 'Professor')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Teacher (teacherID, name, rating, email, office, title)
+               VALUES (5, 'Santosh Chandrasekhar', 5, 'schandrasekhar@example.com', 'Cob2', 'Professor')"""
+        _conn.execute(sql)
+
+        print("Success")
+    except Error as err:
+        print("Error: ", err)
+
+    print(separator)
+
+def insertScheduleData(_conn):
+    print(separator)
+    print("Insert Schedule Data")
+
+    try:
+        # Insert data into Schedule
+        sql = """INSERT INTO Schedule (courseID, day, startTime, endTime)
+              VALUES (1, 'Monday', '09:00', '10:30')"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Schedule (courseID, day, startTime, endTime) VALUES (1, 'Tuesday', '09:00', '10:30')"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Schedule (courseID, day, startTime, endTime) VALUES (2, 'Wednesday', '13:00', '14:30')"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Schedule (courseID, day, startTime, endTime) VALUES (2, 'Thursday', '13:00', '14:30')"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Schedule (courseID, day, startTime, endTime) VALUES (3, 'Friday', '10:30', '13:00')"""
+        _conn.execute(sql)
+
+        _conn.execute("COMMIT")
+        print("Success")
+    except Error as err:
+        print("Error: ", err)
+
+    print(separator)
+
+def insertGradeScaleData(_conn):
+    print(separator)
+    print("Insert GradeScale Data")
+
+    try:
+        # Insert data into GradeScale
+        sql = """INSERT INTO GradeScale (courseID, letterGrade, minimumScore)
+                VALUES (1, 'A', 90),
+                       (1, 'B', 80),
+                       (1, 'C', 70),
+                       (1, 'D', 60),
+                       (1, 'F', 50),
+                       (2, 'A', 89)"""
+        _conn.execute(sql)
+
+        _conn.execute("COMMIT")
+        print("Success")
+    except Error as err:
+        print("Error: ", err)
+
+    print(separator)
+
+def insertTextbookData(_conn):
+    print(separator)
+    print("Insert Textbook Data")
+
+    try:
+        # Insert data into Textbook
+        sql = """        INSERT INTO Textbook (ISBN, name, author) VALUES ('978-3-16-148410-0', 'Python Programming', 'John Doe')"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Textbook (ISBN, name, author) VALUES ('978-3-16-148411-7', 'Advanced Python', 'Jane Smith')"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Textbook (ISBN, name, author) VALUES ('978-3-16-148412-4', 'Data Structures', 'Robert Johnson')"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Textbook (ISBN, name, author) VALUES ('978-3-16-148414-9', 'Discrete Math', 'Frank John')"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Textbook (ISBN, name, author) VALUES ('978-3-16-148415-2', 'Algorithms', 'Jerry Mitt')"""
+        _conn.execute(sql)
+
+        _conn.execute("COMMIT")
+        print("Success")
+    except Error as err:
+        print("Error: ", err)
+
+    print(separator)
+
+def insertStudentData(_conn):
+    print(separator)
+    print("Insert Student Data")
+
+    try:
+        # Insert data into Student
+        sql = """INSERT INTO Student (studentID, name, email)
+              VALUES (1, 'Person One', 'personone@example.com')"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Student (studentID, name, email)
+              VALUES (2, 'Person Two', 'persontwo@example.com')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Student (studentID, name, email)
+              VALUES (3, 'Person Three', 'personthree@example.com')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Student (studentID, name, email)
+              VALUES (4, 'Person Four', 'personfour@example.com')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Student (studentID, name, email)
+              VALUES (5, 'Person Five', 'personfive@example.com')"""
+        _conn.execute(sql)
+
+        print("Success")
+    except Error as err:
+        print("Error: ", err)
+
+    print(separator)
+
+
+def insertEnrollmentData(_conn):
+    print(separator)
+    print("Insert Enrollment Data")
+
+    try:
+        # Insert data into Enrollment
+        sql = """INSERT INTO Enrollment (courseID, studentID)
+              VALUES (1, 1)"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Enrollment (courseID, studentID)
+              VALUES (2, 2)"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Enrollment (courseID, studentID)
+              VALUES (3, 3)"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Enrollment (courseID, studentID)
+              VALUES (4, 4)"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Enrollment (courseID, studentID)
+              VALUES (5, 5)"""
+        _conn.execute(sql)
+
+        print("Success")
+    except Error as err:
+        print("Error: ", err)
+
+    print(separator)
+
+def insertCourseTextbookData(_conn):
+    print(separator)
+    print("Insert CourseTextbook Data")
+
+    try:
+        # Insert data into CourseTextbook
+        sql = """INSERT INTO CourseTextbook (courseID, ISBN)
+              VALUES (1, '978-3-16-148410-0')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO CourseTextbook (courseID, ISBN)
+              VALUES (2, '978-3-16-148411-7')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO CourseTextbook (courseID, ISBN)
+              VALUES (3, '978-3-16-148412-4')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO CourseTextbook (courseID, ISBN)
+              VALUES (4, '978-3-16-148414-9')"""
+        _conn.execute(sql)
+        sql = """INSERT INTO CourseTextbook (courseID, ISBN)
+              VALUES (5, '978-3-16-148415-2')"""
+        _conn.execute(sql)
+
+        print("Success")
+    except Error as err:
+        print("Error: ", err)
+
+    print(separator)
+
+def insertPrereqData(_conn):
+    print(separator)
+    print("Insert Prereq Data")
+
+    try:
+        # Insert data into Prereq
+        sql = """INSERT INTO Prereq (courseID, prereqID)
+              VALUES (2, 1)"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Prereq (courseID, prereqID) VALUES (1, null)"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Prereq (courseID, prereqID) VALUES (3, 1)"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Prereq (courseID, prereqID) VALUES (3, 2)"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Prereq (courseID, prereqID) VALUES (4, 3)"""
+        _conn.execute(sql)
+
+        _conn.execute("COMMIT")
+        print("Success")
+    except Error as err:
+        print("Error: ", err)
+
+    print(separator)
+
+def insertCoreqData(_conn):
+    print(separator)
+    print("Insert Coreq Data")
+
+    try:
+        # Insert data into Coreq
+        sql = """INSERT INTO Coreq (courseID, coreqID) VALUES (5, 6)"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Coreq (courseID, coreqID) VALUES (7, 8)"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Coreq (courseID, coreqID) VALUES (9, 8)"""
+        _conn.execute(sql)
+        sql = """INSERT INTO Coreq (courseID, coreqID) VALUES (10, 11)"""
+        _conn.execute(sql)
+
+        sql = """INSERT INTO Coreq (courseID, coreqID) VALUES (12, 13)"""
+        _conn.execute(sql)
+
+        _conn.execute("COMMIT")
+        print("Success")
+    except Error as err:
+        print("Error: ", err)
+
+    print(separator)
+
+def populateTable(_conn):
+    insertCourseData(_conn)
+    insertTeacherData(_conn)
+    insertScheduleData(_conn)
+    insertGradeScaleData(_conn)
+    insertTextbookData(_conn)
+    insertStudentData(_conn)
+    insertEnrollmentData(_conn)
+    insertCourseTextbookData(_conn)
+    insertPrereqData(_conn)
+    insertCoreqData(_conn)
+
 # Create a funciton for each sqlite query we want to do
 def main():
     database = r"courses.sql"
 
     conn = openConnection(database)
-    with conn:
+    # with conn:
     # Call function for querys
     #     createTables(conn)
     #     dropTables(conn)
+    #     populateTable(conn)
     closeConnection(conn, database)
 
 
